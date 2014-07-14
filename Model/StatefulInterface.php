0e2e83e962bda2a46b20ed5b84064333bdb9d4cb
Piotr Minkina <p.minkina@piotrminkina.pl>
2014-07-14 20:49:23 +0200

    Initialized base interface to store and retrieve current model state

---
+++ Model/StatefulInterface.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the PMDStateMachineBundle package.
+ *
+ * (c) Piotr Minkina <projekty@piotrminkina.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PMD\StateMachineBundle\Model;
+
+/**
+ * Interface StatefulInterface
+ * 
+ * @author Piotr Minkina <projekty@piotrminkina.pl>
+ * @package PMD\StateMachineBundle\Model
+ */
+interface StatefulInterface
+{
+    /**
+     * @param string $state
+     * @return $this
+     */
+    public function setState($state);
+
+    /**
+     * @return $this
+     */
+    public function getState();
+}
