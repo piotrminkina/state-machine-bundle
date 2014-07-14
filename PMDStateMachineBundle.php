4f589360230b7b3a7fd51c5268c042fea7d11879
Piotr Minkina <p.minkina@piotrminkina.pl>
2014-07-14 07:38:10 +0200

    Added missing bundle class

---
+++ PMDStateMachineBundle.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the PMDStateMachineBundle package.
+ *
+ * (c) Piotr Minkina <projekty@piotrminkina.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PMD\StateMachineBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class PMDStateMachineBundle
+ * 
+ * @author Piotr Minkina <projekty@piotrminkina.pl>
+ * @package PMD\StateMachineBundle
+ */
+class PMDStateMachineBundle extends Bundle
+{
+}
