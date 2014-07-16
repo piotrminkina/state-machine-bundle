4be0d67f75fdf362fcf186197bd9814fb4aadbb1
Piotr Minkina <p.minkina@piotrminkina.pl>
2014-07-16 21:09:05 +0200

    Changed builder interface and changed its namespace

---
+++ Process/Builder/DefinitionBuilderInterface.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * This file is part of the PMDStateMachineBundle package.
+ *
+ * (c) Piotr Minkina <projekty@piotrminkina.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PMD\StateMachineBundle\Process\Builder;
+
+use PMD\StateMachineBundle\Process\DefinitionInterface;
+
+/**
+ * Interface DefinitionBuilderInterface
+ *
+ * @author Piotr Minkina <projekty@piotrminkina.pl>
+ * @package PMD\StateMachineBundle\Process\Builder
+ */
+interface DefinitionBuilderInterface
+{
+    /**
+     * @param string $name
+     * @return $this
+     */
+    public function addState($name);
+
+    /**
+     * @param string $name
+     * @return $this
+     */
+    public function addTransition($name);
+
+    /**
+     * @param string $sourceState
+     * @param string $transition
+     * @param string $targetState
+     * @return $this
+     */
+    public function linkStates($sourceState, $transition, $targetState);
+
+    /**
+     * @param string $sourceStateState
+     * @param string $transition
+     * @return $this
+     */
+    public function linkSourceState($sourceStateState, $transition);
+
+    /**
+     * @param string $transition
+     * @param string $targetStateState
+     * @return $this
+     */
+    public function linkTargetState($transition, $targetStateState);
+
+    /**
+     * @return DefinitionInterface
+     */
+    public function getDefinition();
+}
