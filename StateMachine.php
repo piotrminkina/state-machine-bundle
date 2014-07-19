9c69940485617260e93039bfff47a7dffac288f0
Piotr Minkina <p.minkina@piotrminkina.pl>
2014-07-19 21:57:53 +0200

    Removed abstract state machine

--- StateMachine.php
+++ StateMachine.php
@@ -11,12 +11,113 @@
 
 namespace PMD\StateMachineBundle;
 
+use PMD\StateMachineBundle\Process\StateInterface;
+use PMD\StateMachineBundle\Process\TransitionInterface;
+use PMD\StateMachineBundle\Model\StatefulInterface;
+
 /**
  * Class StateMachine
  * 
  * @author Piotr Minkina <projekty@piotrminkina.pl>
  * @package PMD\StateMachineBundle
  */
-class StateMachine extends AbstractStateMachine
+class StateMachine implements StateMachineInterface
 {
+    /**
+     * @var StatefulInterface
+     */
+    protected $object;
+
+    /**
+     * @var StateMachineCoordinatorInterface
+     */
+    protected $coordinator;
+
+    /**
+     * @var StateInterface
+     */
+    protected $state;
+
+    /**
+     * @var TransitionInterface[]
+     */
+    protected $transitions;
+
+    /**
+     * @param StatefulInterface $object
+     * @param StateMachineCoordinatorInterface $coordinator
+     */
+    public function __construct(
+        StatefulInterface $object,
+        StateMachineCoordinatorInterface $coordinator
+    ) {
+        $this->object = $object;
+        $this->coordinator = $coordinator;
+
+        $this->updateMachine($object);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getTransitions()
+    {
+        return $this->transitions;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function hasTransition($label)
+    {
+        return isset($this->transitions[$label]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function transit($label, $inputData = null)
+    {
+        if (!isset($this->transitions[$label])) {
+            throw new \Exception('Cannot flow by %s, because transition of this label doesn\'t exits');
+        }
+        $transition = $this->transitions[$label];
+        $outputData = $this->coordinator->transit($transition, $inputData);
+
+        if ($this->coordinator->isCompleted()) {
+            $state = $transition->getTargetState();
+            $this->updateObject($state);
+            $this->updateMachine($this->object);
+        }
+
+        return $outputData;
+    }
+
+    /**
+     * @param StateInterface $state
+     */
+    protected function updateObject(StateInterface $state)
+    {
+        $this->object->setState($state->getName());
+    }
+
+    /**
+     * @param StatefulInterface $object
+     */
+    protected function updateMachine(StatefulInterface $object)
+    {
+        $stateName = $object->getState();
+        $coordinator = $this->coordinator;
+
+        $this->state = $coordinator->getStateObject($stateName);
+        $this->transitions = $coordinator->getAllowedTransitions($this->state);
+    }
 }
