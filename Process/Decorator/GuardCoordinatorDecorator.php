f2ae3fb3bad894d02d8c61005d6ec3cd133d25a4
Piotr Minkina <p.minkina@piotrminkina.pl>
2014-07-21 23:38:53 +0200

    Implemented decorator for checks security access

--- Process/Decorator/GuardCoordinatorDecorator.php
+++ Process/Decorator/GuardCoordinatorDecorator.php
@@ -11,6 +11,13 @@
 
 namespace PMD\StateMachineBundle\Process\Decorator;
 
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Core\SecurityContextInterface;
+use PMD\StateMachineBundle\Process\CoordinatorInterface;
+use PMD\StateMachineBundle\Process\TokenInterface;
+use PMD\StateMachineBundle\Security\Authorization\Voter\AbstractTokenVoter;
+use PMD\StateMachineBundle\StateMachineInterface;
+
 /**
  * Class GuardCoordinatorDecorator
  * 
@@ -19,4 +26,50 @@ namespace PMD\StateMachineBundle\Process\Decorator;
  */
 class GuardCoordinatorDecorator extends AbstractCoordinatorDecorator
 {
+    /**
+     * @var SecurityContextInterface
+     */
+    protected $security;
+
+    /**
+     * @param CoordinatorInterface $coordinator
+     * @param SecurityContextInterface $security
+     */
+    public function __construct(
+        CoordinatorInterface $coordinator,
+        SecurityContextInterface $security
+    ) {
+        parent::__construct($coordinator);
+
+        $this->security = $security;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getTokens(StateMachineInterface $instance, $context)
+    {
+        $tokens = parent::getTokens($instance, $context);
+        $limited = array();
+
+        foreach ($tokens as $name => $token) {
+            if ($this->security->isGranted(AbstractTokenVoter::CREATE, $token)) {
+                $limited[$name] = $token;
+            }
+        }
+
+        return $limited;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function consume(TokenInterface $token, $data = null)
+    {
+        if (!$this->security->isGranted(AbstractTokenVoter::CONSUME, $token)) {
+            throw new AccessDeniedException();
+        }
+
+        return parent::consume($token, $data);
+    }
 }
